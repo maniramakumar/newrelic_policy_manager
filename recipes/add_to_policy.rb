616ec83237d87a46ff13c2738537bcb3a5abc135
Elliot Hursh <elliothursh@gmail.com>
2014-12-12 17:21:48 -0600

    fix?

--- recipes/add_to_policy.rb
+++ recipes/add_to_policy.rb
@@ -1,4 +1,4 @@
 include_recipe "newrelic_policy_manager::default"
 
-server = NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
+server = ::NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
 server.add_to_policy(node[:newrelic][:policy_manager][:startup_policy])
