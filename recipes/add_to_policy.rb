28d6ebba036213db0aa24d1366caaf503cd5f290
Elliot Hursh <elliothursh@gmail.com>
2014-12-12 22:16:22 -0600

    another test

--- recipes/add_to_policy.rb
+++ recipes/add_to_policy.rb
@@ -1,3 +1,4 @@
+require File.expand_path("../../libraries/newrelic_server.rb", __FILE__)
 include_recipe "newrelic_policy_manager::default"
 
 server = Chef::Recipe::NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
