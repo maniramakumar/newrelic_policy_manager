baf0292c76f84d911f683e36c96f5e80aaaede29
Elliot Hursh <elliothursh@gmail.com>
2014-12-14 21:32:44 -0600

    putting code in a ruby block to delay execution

--- recipes/add_to_policy.rb
+++ recipes/add_to_policy.rb
@@ -1,5 +1,10 @@
 require File.expand_path("../../libraries/newrelic_server.rb", __FILE__)
 include_recipe "newrelic_policy_manager::default"
 
-server = Chef::Recipe::NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
-server.add_to_policy(node[:newrelic][:policy_manager][:startup_policy])
+ruby_block "add_server_to_policy" do
+  block do
+    server = Chef::Recipe::NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
+    server.add_to_policy(node[:newrelic][:policy_manager][:startup_policy])
+  end
+end
+
--- recipes/remove_from_policy.rb
+++ recipes/remove_from_policy.rb
@@ -1,5 +1,10 @@
 require File.expand_path("../../libraries/newrelic_server.rb", __FILE__)
 include_recipe "newrelic_policy_manager::default"
 
-server = Chef::Recipe::NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
-server.add_to_policy(node[:newrelic][:policy_manager][:shutdown_policy])
+ruby_block "remove_server_from_policy" do
+  block do
+    server = Chef::Recipe::NewrelicServer.new(node[:newrelic][:apikey],node[:newrelic][:policy_manager][:server_name])
+    server.add_to_policy(node[:newrelic][:policy_manager][:shutdown_policy])
+  end
+end
+
