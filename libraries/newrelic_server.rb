7d7258e1679d9d389ab77880434c8e72821ad0e5
Elliot Hursh <elliothursh@gmail.com>
2014-12-18 19:31:59 -0600

    Changed an && to an ||

--- libraries/newrelic_server.rb
+++ libraries/newrelic_server.rb
@@ -18,7 +18,7 @@ class Chef
         if response.code != 200
           log_error(response.code)
         else 
-          Chef::Log.info("Successfully deleted #{@server_name}")
+          Chef::Log.info "Successfully deleted #{@server_name}"
         end
       end
 
@@ -31,12 +31,12 @@ class Chef
           log_error(response.code)
         else 
           if response["servers"].length <= 0
-            Chef::Log.error("Received 0 servers with the name #{@server_name}, expected exactly one")
-            {}
+            Chef::Log.error "Received 0 servers with the name #{@server_name}, expected exactly one"
           else
-            response["servers"].first
+            return response["servers"].first
           end
         end
+        {}
       end
       private :get_by_name
 
@@ -48,23 +48,23 @@ class Chef
         if response.code != 200
           log_error(response.code)
         else 
-          if response["alert_policies"].length <= 0
-            CHef::log.error("Received 0 policies with the name #{policy_name}, expected exactly one")
-            {}
+          alert_policy = response["alert_policies"].select{ |alert_policy| alert_policy["name"] == policy_name }
+          if alert_policy.length <= 0
+            Chef::Log.error "Received 0 policies with the name #{policy_name}, expected exactly one"
           else
-            response["alert_policies"].select{ |alert_policy| alert_policy["name"] == policy_name }.first
+            return alert_policy.first
           end
         end
+        {}
       end
       private :get_policy_by_name
 
       def add_to_policy(policy_name)
         @server_response = get_by_name
         @policy_response = get_policy_by_name(policy_name)
-
-        unless @server_response.empty? && @policy_response.empty?
+        unless @server_response.empty? || @policy_response.empty?
           if server_ids.include? server_id
-            Chef::Log.warn("Server #{@server_name} already belongs to #{policy_name}")
+            Chef::Log.info "Server #{@server_name} already belongs to #{policy_name}"
           else
             servers = server_ids << server_id
             response = HTTParty.put(
@@ -81,10 +81,10 @@ class Chef
             if response.code != 200
               log_error(response.code)
             else 
-              if response["alert_policy"]["links"]["servers"].empty?
-                Chef::Log.warn response
+              if @policy_response["links"] && !@policy_response["links"]["servers"].include? server_id
+                Chef::Log.error "Failed to added server #{@server_name} to alert policy with name #{policy_name}"
               else 
-                Chef::Log.info "Successfully added server #{@server_name} to alert policy with id #{policy_name}"
+                Chef::Log.info "Successfully added server #{@server_name} to alert policy with name #{policy_name}"
               end
             end
           end
@@ -102,22 +102,24 @@ class Chef
       private :server_id
 
       def server_ids
-        @policy_response["links"]["servers"] || []
+        if @policy_response["links"] && @policy_response["links"]["servers"]
+          @policy_response["links"]["servers"]
+        else
+          []
+        end
       end
       private :server_ids
 
       def log_error(code) 
         case code
-        when 401 then Chef::Log.warn("Invalid or missing NewRelic API code")
-        when 403 then Chef::Log.warn("New Relic API access has not been enabled for your account")
-        when 404 then Chef::Log.warn("No alert policy found with the given ID")
-        when 422 then Chef::Log.warn("Validation error occurred when updating the alert policy")
-        when 500 then Chef::Log.warn("A server error occured, please contact New Relic support")
-        else Chef::Log.warn("Unknown response code #{code}") end
+        when 401 then Chef::Log.error "Invalid or missing NewRelic API code"
+        when 403 then Chef::Log.error "New Relic API access has not been enabled for your account"
+        when 404 then Chef::Log.error "No alert policy found with the given ID"
+        when 422 then Chef::Log.error "Validation error occurred when updating the alert policy"
+        when 500 then Chef::Log.error "A server error occured, please contact New Relic support"
+        else Chef::Log.error "Unknown response code #{code}" end
       end
 
     end
   end
 end
-
-
